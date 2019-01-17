8b4ce40f8348cd35c5eb9660d65a30c4e13edce7
ImNotAVirus <s.soulie@hotmail.fr>
2019-01-17 15:16:54 +0100

    :sparkles: Add metadata into 'Client' structure

--- apps/elven_gard/lib/elven_gard/game/client.ex
+++ apps/elven_gard/lib/elven_gard/game/client.ex
@@ -3,18 +3,21 @@ defmodule ElvenGard.Game.Client do
   Documentation for ElvenGard.Game.Client.
   """
 
-  @keys [:id, :socket, :transport]
+  @keys [:id, :socket, :transport, :metadata]
   @enforce_keys @keys
   defstruct @keys
 
   @type t :: %__MODULE__{}
+  @type metadata_key :: [term, ...] | term
+  @type metadata_value :: term | nil
 
-  @spec new(identifier, atom) :: __MODULE__.t()
-  def new(socket, transport) do
+  @spec new(identifier, atom, map) :: __MODULE__.t()
+  def new(socket, transport, metadata \\ %{}) do
     %__MODULE__{
       id: UUID.uuid4(),
       socket: socket,
-      transport: transport
+      transport: transport,
+      metadata: metadata
     }
   end
 
@@ -26,5 +29,28 @@ defmodule ElvenGard.Game.Client do
     } = client
 
     transport.send(socket, message)
+  end
+
+  @spec get_metadata(__MODULE__.t(), metadata_key) :: metadata_value
+  def get_metadata(client, [_ | _] = key), do: get_in(client.metadata, key)
+  def get_metadata(client, key), do: Map.get(client.metadata, key)
+
+  @spec set_metadata(__MODULE__.t(), metadata_key, metadata_value) :: __MODULE__.t()
+  def set_metadata(client, [_ | _] = key, value) do
+    %__MODULE__{
+      metadata: metadata
+    } = client
+
+    new_meta = put_in(metadata, key, value)
+    %__MODULE__{ client | metadata: new_meta }
+  end
+
+  def set_metadata(client, key, value) do
+    %__MODULE__{
+      metadata: metadata
+    } = client
+
+    new_meta = Map.put(metadata, key, value)
+    %__MODULE__{ client | metadata: new_meta }
   end
 end
