85b78523457ccf57c2c3af0388f44014b288afba
ImNotAVirus <s.soulie@hotmail.fr>
2019-02-23 13:53:19 +0100

    :recycle: Some refactoring

--- apps/elven_gard/lib/elven_gard/structures/field_documentation.ex
+++ apps/elven_gard/lib/elven_gard/structures/field_documentation.ex
@@ -3,17 +3,22 @@ defmodule ElvenGard.Structures.FieldDocumentation do
   Structure for a packet field's documentation
   """
 
-  @keys [:name, :type, :doc]
+  @keys [:name, :type, :description]
   @enforce_keys @keys
   defstruct @keys
 
   @type t :: %__MODULE__{}
+  @type empty_string :: String.t() | nil
 
   @doc """
   Create a new structure
   """
-  @spec new(String.t(), atom, String.t()) :: __MODULE__.t()
-  def new(name, type, doc) do
-    %__MODULE__{name: name, type: type, doc: doc}
+  @spec new(String.t(), atom, empty_string) :: __MODULE__.t()
+  def new(name, type, desc) do
+    %__MODULE__{
+      name: name,
+      type: type,
+      description: (if desc, do: String.trim(desc), else: desc)
+    }
   end
 end
