79c64668f3baf659bf07b7ef519628100b8dd860
DarkyZ aka NotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2020-11-18 15:56:33 +0100

    :sparkles: Export formatter options

--- .formatter.exs
+++ .formatter.exs
@@ -1,11 +1,14 @@
 # Used by "mix format"
+locals_without_parens = [
+  packet: 2,
+  field: 2,
+  field: 3,
+  resolve: 1,
+  ignore_packet: 1
+]
+
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    packet: 2,
-    field: 2,
-    field: 3,
-    resolve: 1,
-    ignore_packet: 1
-  ]
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
