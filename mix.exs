1e53dfc185d41f615a400c44855f4a06244ba726
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2023-02-28 12:19:26 +0100

    :sparkles: Setup ExCoveralls

--- mix.exs
+++ mix.exs
@@ -14,7 +14,14 @@ defmodule ElvenGard.Network.MixProject do
       description: "MMORPG Game Server toolkit written in Elixir",
       package: package(),
       docs: docs(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
