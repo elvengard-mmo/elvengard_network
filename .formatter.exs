0fa7be84416365801e7cabf9af1b633017742030
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2023-07-21 13:59:05 +0200

    :sparkles: Add locals_without_parens in formatter

--- .formatter.exs
+++ .formatter.exs
@@ -6,6 +6,16 @@ inputs =
     &Path.wildcard(&1, match_dot: true)
   ) -- ["lib/elven_gard/network/uuid.ex"]
 
+locals_without_parens = [
+  field: 2,
+  field: 3,
+  packet: 1,
+  packet: 2,
+  packet: 3
+]
+
 [
-  inputs: inputs
+  inputs: inputs,
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
