acdf7256b6f85e7aa615e700a04ee39bc49d23b6
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2023-07-27 22:08:43 +0200

    :memo: Add documentation for `ElvenGard.Network.View`

--- lib/elven_gard/network/view.ex
+++ lib/elven_gard/network/view.ex
@@ -1,12 +1,48 @@
 defmodule ElvenGard.Network.View do
   @moduledoc ~S"""
-  Define a custom behaviour for a view (packet sent from server to client)
+  Define a custom behaviour for a View (packet sent from server to client).
+
+  This module defines a behaviour that allows users to create custom packet views
+  to be sent from the server to the client. By implementing the callback defined
+  in this module, users can define how a specific packet view is rendered and
+  converted into binary data for transmission.
+
+  To implement a custom view, you need to define the `render/2` callback. The
+  `render/2` function takes the name of the packet view and a map of parameters
+  as input and returns the binary data to be sent to the client.
+
+  ## Example
+
+  Here's an example of defining a custom view for rendering a login response
+  packet:
+
+      defmodule MyLoginResponseView do
+        use ElvenGard.Network.View
+
+        @impl ElvenGard.Network.View
+        def render(:login_response, %{status: status, message: message}) do
+          <<status::little-unsigned-integer-size(8), message::binary>>
+        end
+      end
+
+  In the above example, we defined a custom view module `MyLoginResponseView` that
+  implements the `render/2` callback for rendering a login response packet.
+
+  Then, when sending a login response packet using `ElvenGard.Network.PacketView`, you
+  can use this custom view to render the packet before sending it to the client.
   """
 
   alias ElvenGard.Network.UnknownViewError
 
   @doc """
-  Build a packet to send to the client
+  Build a packet to send to the client.
+
+  Arguments:
+
+  - `name`: A unique identifier packet view (string or atom).
+  - `params`: A map or keyword list of parameters to be used when rendering the view.
+
+  The function should return an iodata to be sent to the client.
   """
   @callback render(name, params) :: iodata()
             when name: atom() | String.t(), params: map() | Keyword.t()
