460082c5608a358e86be055ef2370e0899e77299
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2025-08-13 08:17:39 +0200

    :ambulance: Apply encode to default values

--- lib/elven_gard/network/packet_serializer.ex
+++ lib/elven_gard/network/packet_serializer.ex
@@ -239,8 +239,8 @@ defmodule ElvenGard.Network.PacketSerializer do
         case Keyword.get(opts, :if) do
           nil ->
             quote location: :keep do
-              case {Map.fetch!(var!(packet), unquote(name)), unquote(opts)[:default]} do
-                {nil, value} when not is_nil(value) -> value
+              case {Map.get(var!(packet), unquote(name)), unquote(opts)[:default]} do
+                {nil, value} when not is_nil(value) -> unquote(type).encode(value, unquote(opts))
                 {value, _} -> unquote(type).encode(value, unquote(opts))
               end
             end
@@ -250,9 +250,12 @@ defmodule ElvenGard.Network.PacketSerializer do
               if unquote(condition) in [nil, false] do
                 :"$drop"
               else
-                case {Map.fetch!(var!(packet), unquote(name)), unquote(opts)[:default]} do
-                  {nil, value} when not is_nil(value) -> value
-                  {value, _} -> unquote(type).encode(value, unquote(opts))
+                case {Map.get(var!(packet), unquote(name)), unquote(opts)[:default]} do
+                  {nil, value} when not is_nil(value) ->
+                    unquote(type).encode(value, unquote(opts))
+
+                  {value, _} ->
+                    unquote(type).encode(value, unquote(opts))
                 end
               end
             end
